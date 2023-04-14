90bbd4f4e3b34fc2726e1bac5def768cb6cd248d
piter <brpiotrwojtowicz@gmail.com>
2023-04-14 10:22:16 +0200

    html pages added

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
+
+#--------> For admin
+@auth.route('/login')
+def login():
+    return render_template('login.html')
--- website/views.py
+++ website/views.py
@@ -2,6 +2,30 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
+#--------> For client
 @views.route('/')
 def home():
-    return render_template('home.html') 
+    return render_template('home.html')
+
+@views.route('/aftersignup')
+def after_sign_up():
+    return render_template('aftersignup.html')
+
+
+#--------> For admin
+@views.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@views.route('/dashboard/signup')
+def admin_sign_up():
+    return render_template('adminsignup.html')
+
+@views.route('/dashboard/events')
+def admin_events():
+    return render_template('adminevents.html') 
+
+@views.route('/dashboard/blacklist')
+def admin_blacklist():
+    return render_template('adminblacklist.html') 
+
